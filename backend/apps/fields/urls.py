3bc6963e9f87e29ebfd77ee4e7351caeb08b1fb8
sigurdskatvedt <60401047+sigurdskatvedt@users.noreply.github.com>
2023-11-01 15:58:08 +0100

    added booking system

--- backend/apps/fields/urls.py
+++ backend/apps/fields/urls.py
@@ -1,13 +1,11 @@
-# apps/fields/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FieldViewSet
+from .views import FieldViewSet, BookFieldView
 
 router = DefaultRouter()
 router.register(r'fields', FieldViewSet, basename='field')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('<int:field_id>/book/', BookFieldView.as_view(), name='book-field'),
 ]
-
--- backend/apps/fields/views.py
+++ backend/apps/fields/views.py
@@ -1,35 +1,29 @@
-# apps/fields/views.py
-
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from .models import Field, Booking
+from django.utils.dateparse import parse_datetime
 
 
-class FieldViewSet(viewsets.ViewSet):
-    @action(detail=True, methods=['post'])
-    def book(self, request, field_id):
-        if request.method == 'POST':
-            # Extract start_time and end_time from the request body
-            start_time = request.data.get('start_time')
-            end_time = request.data.get('end_time')
+class BookFieldView(APIView):
+    def post(self, request, field_id):
+        field = get_object_or_404(Field, pk=field_id)
+        # ... (rest of the implementation)
 
-            field = get_object_or_404(Field, id=field_id)
-            bookings = Booking.objects.filter(field=field)
+        start_time = parse_datetime(request.data.get('start_time'))
+        end_time = parse_datetime(request.data.get('end_time'))
 
-            # Convert start_time and end_time to datetime objects
-            # ...
+        if start_time is None or end_time is None:
+            return Response({'error': 'Invalid start_time or end_time'})
 
-            # Check if the requested time period is available
-            for booking in bookings:
-                if (start_time >= booking.start_time and start_time < booking.end_time) or \
-                   (end_time > booking.start_time and end_time <= booking.end_time):
-                    return Response({'error': 'Field is not available during the requested time period'})
+        bookings = Booking.objects.filter(field=field)
 
-            # Create a new booking
-            new_booking = Booking.objects.create(field=field, start_time=start_time, end_time=end_time)
-            return Response({'message': 'Field booked successfully', 'booking_id': new_booking.id})
-        else:
-            return Response({'error': 'Invalid request method'})
+        for booking in bookings:
+            if (start_time >= booking.start_time and start_time < booking.end_time) or \
+               (end_time > booking.start_time and end_time <= booking.end_time):
+                return Response({'error': 'Field is not available during the requested time period'})
 
+        new_booking = Booking.objects.create(
+            field=field, start_time=start_time, end_time=end_time)
+        return Response({'message': 'Field booked successfully', 'booking_id': new_booking.id})
