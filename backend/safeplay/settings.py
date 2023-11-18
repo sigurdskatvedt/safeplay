1a827fb5bf9ec3495144fa942a7c7de1c4d382b3
sigurdskatvedt <60401047+sigurdskatvedt@users.noreply.github.com>
2023-11-18 14:35:57 +0100

    no password validation

--- backend/safeplay/settings.py
+++ backend/safeplay/settings.py
@@ -144,20 +144,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-]
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
