5346c3ca9f63dce76d4095c0e932d1c8e87d206a
sigurdskatvedt <60401047+sigurdskatvedt@users.noreply.github.com>
2023-11-18 14:18:33 +0100

    everyone can fetch teams

--- backend/apps/teams/views.py
+++ backend/apps/teams/views.py
@@ -2,13 +2,10 @@ from rest_framework import generics, mixins, status
 from rest_framework.response import Response
 from .models import Team
 from .serializers import TeamSerializer
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
 
 class TeamListView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
-
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
