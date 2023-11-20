6c1502f5774d8aa205858fd5cd61bfcc9b3d8398
sigurdskatvedt <60401047+sigurdskatvedt@users.noreply.github.com>
2023-11-20 10:01:34 +0100

    no insecure file types

--- backend/apps/objections/models.py
+++ backend/apps/objections/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-from apps.users.models import User
-from .validators import FileValidator
+from django.core.validators import FileExtensionValidator
 
 
 class Objection(models.Model):
     # The document itself
-    document = models.FileField(upload_to="objections", validators=[FileValidator(
-        allowed_mimetypes='', allowed_extensions='', max_size=1024*1024*5)], blank=False, null=False)
+    document = models.FileField(
+        upload_to="objections",
+        validators=[
+            FileExtensionValidator(allowed_extensions=['pdf'])
+        ],
+        max_length=255,
+        blank=False,
+        null=False
+    )
 
     name = models.CharField(max_length=255)
     text = models.TextField(max_length=3000)
