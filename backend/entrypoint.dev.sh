457080044e8a170b04cf1737035c8046163a6a4e
sigurdskatvedt <60401047+sigurdskatvedt@users.noreply.github.com>
2024-01-11 14:16:48 +0100

    start server with some fields

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -15,6 +15,6 @@ DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL} \
 python manage.py createsuperuser --noinput || \
 echo "WARNING: This error is ignored as it most likely is 'That username is already taken.'" \
 && echo "If you wish to alter the user credentials, then delete the user first."
-#python manage.py loaddata initial.json # Add some initial data
+python manage.py loaddata initial.json # Add some initial data
 
 gunicorn safeplay.wsgi --log-file - -b 0.0.0.0:8000 --reload
